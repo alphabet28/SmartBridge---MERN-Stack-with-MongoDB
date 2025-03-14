a5d81b42daa60293f056f2be39e144a457ad9c46
alphabet28 <atharv.bhavsar2801@gmail.com>
2025-03-14 22:18:59 +0530

    completed task 3

---
+++ Week 2 - JavaScript_Assignment/Task3.js
@@ -0,0 +1,13 @@
+// 3: Write a program that checks if a number n is even or odd and stores the result in a variable. 
+
+let evenNumber
+let oddNumber
+
+let n = 7
+
+if (n % 2 === 0) {
+    evenNumber = n
+} else {
+    oddNumber = n
+}
+console.log(oddNumber);
